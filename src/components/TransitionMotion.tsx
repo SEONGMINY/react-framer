7890457fa101056376e3052bde27a2fadad68b45
이성민 <leesm2075@naver.com>
2023-09-04 15:07:09 +0900

    feat: transition 컴포넌트

---
+++ src/components/TransitionMotion.tsx
@@ -0,0 +1,19 @@
+import "../styles/baseStyles.css";
+import { motion } from "framer-motion";
+
+const TransitionMotion = () => {
+  return (
+    <motion.div
+      className="box"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01],
+      }}
+    />
+  );
+};
+
+export default TransitionMotion;
